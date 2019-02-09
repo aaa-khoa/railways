7f8b3d05e8b41a0a7458a99671477a89ac05a446
Desmond Pompa Alarcon Rawls <drawls@pivotal.io>
2019-02-09 13:28:41 -0800

    initial commit

---
+++ lib/Result.test.ts
@@ -0,0 +1,5 @@
+describe('Result', () => {
+  it('works', () => {
+    expect(1).toEqual(2)
+  })
+})
---
+++ lib/Result.ts
@@ -0,0 +1,7 @@
+
+export type FailFastResult<Value, Error> = Success<Value> | Failure<Error>
+
+export const Success<Value> = {
+
+
+
