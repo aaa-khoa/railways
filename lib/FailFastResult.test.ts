81173ced45b2083d94c62d6edc2c37f05ab12919
Desmond Pompa Alarcon Rawls <drawls@pivotal.io>
2019-02-09 23:03:22 -0800

    this is all completely broken nonsense.

--- lib/FailFastResult.test.ts
+++ lib/FailFastResult.test.ts
@@ -1,5 +1,13 @@
+import { Success, Failure } from './FailFastResult.ts'
+
+type ValidationErrors = 'weird name' | 'cant type' | 'martian'
+
+const validateWeirdName = ({ name } as driver) => name.indexOf('ochocinco') > -1 ? Failure<ValidationErrors>('weird name') : Success(driver)
+const validateState = ({ state } as driver) => state.indexOf('*') > -1 ? Failure<ValidationErrors>('cant type') : Success(driver)
+
 describe('FailFastResult', () => {
   it('works', () => {
-    expect(1).toEqual(2)
+    const result = validateWeirdName({name: 'chad ochocinco', state: 'A*Z'}).map(validateState)
+    expect(result.fold((x) => x, (x) => x)).toEqual('weird name')
   })
 })
